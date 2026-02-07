ea787d7ed9208759aa1c50ad2c7e4ae011f33106
shruti11-web <shrutisingh5630@gmail.com>
2026-02-08 02:09:34 +0530

    Add files via upload
    Day 2 :  Added Python variables , data types, and input code

---
+++ day1_var_DT.py
@@ -0,0 +1,51 @@
+name="Shinchan"
+age = 5
+course = "ML with Data Science"
+print("hello, my name is " + name + " I am " + str(age) + " years old ." + "\nhere, I am learning " + course +".")
+# Multiple Assigning
+a = b = c =100
+print(a,b,c)
+#Assigning Different Values
+a , b, c = "Ram" , 3 , 3.7
+print(a , b, c)
+
+# for adding 
+print("Adittion")
+a=2
+b=3
+print(a+b)
+
+a=5
+b="hii"
+c=3.8
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#sequence of datatype
+s = "Welcome to Geeks World"
+print(s)
+print(type(s))
+print(s[1])
+print(s[5])
+
+print("Practice Set")
+#python program for remainder
+
+a=34
+b=5
+print("remainder is" ,a%b)
+
+a=input("value of a is :" )
+print(type(a))
+type(a)
+
+a=int(input("enter value of a: "))
+b=int(input("enter value of b: "))
+
+print("a i grater then b is" ,a>b)
+
+print("average of no.s is" , (a+b)/2)
+print("square of a", a**2)
