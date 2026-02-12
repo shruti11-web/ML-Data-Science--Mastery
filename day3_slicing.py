b54c88eeffce3ff40c4cfaf685df2467f3300f20
shruti11-web <shrutisingh5630@gmail.com>
2026-02-12 17:27:58 +0530

    Add files via upload

---
+++ day3_slicing.py
@@ -0,0 +1,32 @@
+a="Hello World"
+print(a[2:5])
+print(a[:5])
+print(a[2:8])
+print(a[2:])
+print(a[-5:-2])
+
+#Modify String
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(len(a))
+#Replace String
+print(a.replace("H","T"))
+print(a.split("W"))
+print(a.split("l"))
+print(a.split(" "))
+
+
+#String Concatenation
+c="hii"
+d="Sam"
+e=c+d
+print(e)
+f= c+" "+d
+print(f)
+
+#String Format
+age=36
+print("my name is John, I am " + str(age) + " years old.")
+
+
